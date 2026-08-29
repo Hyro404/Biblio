8b19dcca8b3b9e6db76513155a37f1fe1c639355
Isidora Villagrán <isidoravillagran23@gmail.com>
2026-08-28 23:45:10 -0400

    Prototipo

--- app/page.js
+++ app/page.js
@@ -1,69 +1,154 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
 export default function Home() {
+  const[ventanita, setVentanita] = useState(false);
+  const[estante, setEstante] = useState(false);
+  const[leyendo, setLeyendo] = useState(false);
+  const[ProxLectura, setProxLec] = useState(false);
+  const[logros, setLogros] = useState(false);
+  const[mensajes, setMens] = useState(false);
+  const[sugerencia, setSugerencia] = useState(false);
+  const[aleatorio, setAleatorio] = useState(false);
+  const[mvalorados, setMValorados] = useState(false);
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.js
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+    <main>
+      <h1>TITULO...</h1>
+
+      <button className = "b_catalogo" onClick={()=>setVentanita(true)}>
+        <span>Disponible</span>
+        <img src="/imagen/catalogo.png" alt= "Catalogo"/>
+      </button>
+
+      {ventanita && (
+        <div className="ventanita">
+          <h2>Buscar</h2>
+
+          <button className="cerrar" onClick={()=>setVentanita(false)}>
+            Cerrar
+          </button>
+      </div>
+      )}
+
+
+      <button className="b_estante" onClick={()=>setEstante(true)}>
+        <span>Biblioteca</span>
+        <img src="/imagen/estante.png" alt= "Estante"/>
+      </button>
+
+      {estante && (
+        <div className="estantecito">
+          <button className="leyendo" onClick={()=>setLeyendo(true)}>
+            Leyendo...
+          </button>
+
+          <button className="proxLectura" onClick={()=>setProxLec(true)}>
+            Próxima lectura
+          </button>
+
+          <button className="cerrar" onClick={()=> setEstante(false)}>
+            Cerrar
+          </button>
     </div>
+    )}
+
+      {leyendo && ( 
+        <div className="vent_leyendo">
+        <h3>Libros actuales: </h3>
+
+        <button className="cerrar" onClick={() => setLeyendo(false)}>
+          Cerrar
+        </button>
+      </div>
+    )}
+
+    {ProxLectura && ( 
+        <div className="vent_proxLect">
+        <h3>Próximos libros: </h3>
+
+        <button className="cerrar" onClick={() => setProxLec(false)}>
+          Cerrar
+        </button>
+      </div>
+    )}
+
+    <button className="b_logros" onClick={()=>setLogros(true)}>
+      <span>Logros ☆</span>
+    <img src="/imagen/logros.png" alt= "logros"/>
+    </button>
+
+    {logros && (
+      <div className="logros">
+        <h5>LOGROS☆</h5>
+
+        <button className="cerrar" onClick={()=>setLogros(false)}>
+            Cerrar
+        </button>
+      </div>
+    )}
+
+    <button className="b_mensajes" onClick={()=>setMens(true)}>
+      <span>Mensajes</span>
+    <img src="/imagen/mensajes.png" alt= "mensajes"/>
+    </button>
+
+    {mensajes && (
+      <div className="mensajes">
+        <h5>Próximamente...</h5>
+
+        <button className="cerrar" onClick={()=>setMens(false)}>
+            Cerrar
+        </button>
+      </div>
+    )}
+
+    <button className="b_sugerencia" onClick={()=>setSugerencia(true)}>
+      <span>Sugerencias</span>
+    <img src="/imagen/libro1.png" alt= "sugerencia"/>
+    </button>
+
+    {sugerencia && (
+      <div className="sugerencia">
+        <h5>Próximamente...</h5>
+
+        <button className="cerrar" onClick={()=>setSugerencia(false)}>
+          Cerrar
+        </button>
+      </div>
+    )}
+
+    <button className="b_aleatorio" onClick={()=>setAleatorio(true)}>
+      <span>Aleatorio 🎲</span>
+    <img src="/imagen/libro2.png" alt= "aleatorio"/>
+    </button>
+
+    {aleatorio && (
+      <div className="aleatorio">
+        <h5>Tu próximo libro será...</h5>
+
+        <button className="cerrar" onClick={()=>setAleatorio(false)}>
+          Cerrar
+        </button>
+      </div>
+    )}
+  
+    <button className="b_mvalorados" onClick={()=>setMValorados(true)}>
+      <span>Mejor valorados 🌟</span>
+    <img src="/imagen/libro3.png" alt= "mvalorados"/>
+    </button>
+
+    {mvalorados && (
+      <div className="mvalorados">
+        <h5>Los mejor valorados son...</h5>
+
+        <button className="cerrar" onClick={()=>setMValorados(false)}>
+          Cerrar
+        </button>
+      </div>
+    )}
+    
+
+
+    </main>
   );
 }
